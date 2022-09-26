493ad656d17abbcd91bded359c4d3c0f208c02d3
Atlas <blinkplaygm@gmail.com>
2022-09-26 12:43:37 -0300

    fixed loading

--- Web - HTML, CSS, JS e Sass/Exercicios/Modulo Bootstrap/Tarefa 1/js/script.js
+++ Web - HTML, CSS, JS e Sass/Exercicios/Modulo Bootstrap/Tarefa 1/js/script.js
@@ -1,17 +1,21 @@
 const loadingBar = document.getElementById("progress-bar");
 const loadingContainer = document.getElementById("loading-container");
+const loadedContent = document.getElementsByTagName("section")[0];
+const headerContent = document.getElementsByTagName("header")[0];
 
 let progress = 0;
 
 var intervalId = setInterval(() => {
   loadingBar.textContent = `${progress}%`;
   if (progress >= 100) {
-    // FINISHED LOADING
     setTimeout(() => {
       loadingContainer.style.opacity = "0";
 
       setTimeout(() => {
         loadingContainer.remove(); // removing element from DOM
+        loadedContent.style.display = "block";
+        console.log(headerContent);
+        headerContent.style.display = "flex";
       }, 300);
     }, 1200);
     clearInterval(intervalId);
